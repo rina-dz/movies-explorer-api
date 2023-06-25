ecb2d1f26d1c34ad8bcd29ee50187bd88aa5de5b
Rina Dz <dz.rina.na@gmail.com>
2023-06-25 18:23:29 +0300

    перенести лимитер после логгера запросов

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors);
-app.use(limiter);
 app.use(helmet());
 
 app.use(requestLogger);
+app.use(limiter);
 app.use(routes);
 app.use(errorLogger);
 
